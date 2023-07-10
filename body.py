7a51523436646e21c9b647f69ddfed0361e5a9cd
Byenna <72694655+Byenna@users.noreply.github.com>
2023-07-10 13:41:45 +0200

    added fletBody

--- body.py
+++ body.py
@@ -1,31 +1,32 @@
-import os
-import openai
-import config
+# import os
+# import openai
+# import config
+# import flet as ft
 
-openai.api_key = config.FIRST_CHAT_BOT
+# openai.api_key = config.FIRST_CHAT_BOT
 
-print("Welcome to the interactive chat! You can type 'quit' to exit.")
+# print("Welcome to the interactive chat! You can type 'quit' to exit.")
 
-while True:
-    user_input = input("User: ")
+# while True:
+#     user_input = input("User: ")
 
-    if user_input.lower() == "quit":
-        break
+#     if user_input.lower() == "quit":
+#         break
 
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {
-                "role": "user",
-                "content": user_input
-            }
-        ],
-        temperature=0.7,
-        max_tokens=100,
-        top_p=1.0,
-        frequency_penalty=0.0,
-        presence_penalty=0.0
-    )
+#     response = openai.ChatCompletion.create(
+#         model="gpt-3.5-turbo",
+#         messages=[
+#             {
+#                 "role": "user",
+#                 "content": user_input
+#             }
+#         ],
+#         temperature=0.7,
+#         max_tokens=100,
+#         top_p=1.0,
+#         frequency_penalty=0.0,
+#         presence_penalty=0.0
+#     )
 
-    assistant_response = response.choices[0].message['content']
-    print("Assistant:", assistant_response)
+#     assistant_response = response.choices[0].message['content']
+#     print("Assistant:", assistant_response)
